d0e423c236c3fdcb2c079db2fbbdaed60562d104
Kasutan <contact@kasutan.pro>
2021-05-04 19:04:14 +0200

    Ajout au panier masqué pour produits avec alcool avec classe vignette

--- inc/actions-vignette-produit.php
+++ inc/actions-vignette-produit.php
@@ -53,6 +53,24 @@ function kasutan_after_shop_loop_item_fermer() {
 add_filter( 'woocommerce_loop_add_to_cart_args', 'kasutan_loop_add_to_cart_args',10,2);
 function kasutan_loop_add_to_cart_args($args, $product ) {
 	$args['attributes']['title']="Ajouter le produit au panier";
-	$args['class']=str_replace('button','',$args['class']);
+	$args['class']=substr_replace($args['class'],'',0,strlen('button')); //on ne remplace que la première occurence au début de la liste de classes
 	return $args;
+}
+
+
+//Ajouter une classe au li.product si le produit se trouve dans la catégorie des produits avec alcool (identifiée dans les options du site)
+add_filter('woocommerce_post_class','kasutan_ajoute_classe_produit_avec_alcool', 10,2);
+function kasutan_ajoute_classe_produit_avec_alcool($classes, $product) {
+	if( function_exists('get_field') && function_exists('kasutan_categories_produit') ) {
+		$categories=kasutan_categories_produit();
+		$categorie_avec_alcool=esc_attr(get_field('categorie_avec_alcool','option')); //renvoie un term_id
+		if(is_array($categories) && !empty($categorie_avec_alcool)) {
+			$sous_categorie=$categories['sous_categorie']->term_id;
+			if($sous_categorie==$categorie_avec_alcool) {
+				$classes[]='avec-alcool';
+			}
+		}
+	}
+
+	return $classes;
 }
