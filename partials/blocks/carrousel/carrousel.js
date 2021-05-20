727416f2d5a4804b2fea18e9445ea6d10f7789e5
Kasutan <contact@kasutan.pro>
2021-05-20 08:57:48 +0200

    Ajouter un titre aux boutons du carrousel

--- partials/blocks/carrousel/carrousel.js
+++ partials/blocks/carrousel/carrousel.js
@@ -1,10 +1,30 @@
-jQuery(".carrousel.owl-carousel").owlCarousel({
-	loop:true,
-	nav : false,
-	dots : true,
-	autoplay:true,
-	autoplayTimeout:5000,
-	autoplaySpeed:2000,
-	autoplayHoverPause:false,
-	items: 1,
-});
+(function($) {
+
+	$( document ).ready(function() {
+		var owl = $(".carrousel.owl-carousel");
+		owl.owlCarousel({
+			loop:true,
+			nav : false,
+			dots : true,
+			autoplay:true,
+			autoplayTimeout:5000,
+			autoplaySpeed:2000,
+			autoplayHoverPause:false,
+			items: 1,
+			checkVisible: false
+		});
+
+		var titreDejaAjoute=false;
+		owl.on('changed.owl.carousel', function(event) {
+			if(titreDejaAjoute) {
+				return;
+			}
+			var dots= $('.owl-dot');
+			$.each(dots, function (indexInArray, valueOfElement) { 
+				$(valueOfElement).attr('title','Afficher la slide '+(indexInArray+1));
+			});
+			titreDejaAjoute=true;
+		})
+
+	}); //fin document ready
+})( jQuery );
